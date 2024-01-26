747d42b4f13639dac2024f02523500f963ec1b9c
Lukas <Lukas@hopae.io>
2024-01-26 11:36:59 +0900

    feat: Add custom verifier in kbJwt
    Signed-off-by: Lukas <Lukas@hopae.io>

--- src/kbjwt.ts
+++ src/kbjwt.ts
@@ -1,7 +1,7 @@
 import { KeyLike } from 'jose';
 import { SDJWTException } from './error';
 import { Jwt } from './jwt';
-import { kbHeader, kbPayload } from './type';
+import { Verifier, kbHeader, kbPayload } from './type';
 
 export class KBJwt<
   Header extends kbHeader = kbHeader,
@@ -19,6 +19,20 @@ export class KBJwt<
       throw new SDJWTException('Invalid Key Binding Jwt');
     }
     return await super.verify(publicKey);
+  }
+
+  public async verifyWithVerifier(verifier: Verifier) {
+    if (
+      !this.header?.alg ||
+      !this.header.typ ||
+      !this.payload?.iat ||
+      !this.payload?.aud ||
+      !this.payload?.nonce ||
+      !this.payload?._sd_hash
+    ) {
+      throw new SDJWTException('Invalid Key Binding Jwt');
+    }
+    return await super.verifyWithVerifier(verifier);
   }
 
   public static fromKBEncode<
--- src/test/kbjwt.spec.ts
+++ src/test/kbjwt.spec.ts
@@ -1,6 +1,6 @@
 import { SDJWTException } from '../error';
 import { KBJwt } from '../kbjwt';
-import { KB_JWT_TYP, SDJWTCompact } from '../type';
+import { KB_JWT_TYP, SDJWTCompact, Verifier } from '../type';
 import Crypto from 'node:crypto';
 
 describe('KB JWT', () => {
@@ -96,6 +96,70 @@ describe('KB JWT', () => {
     const decoded = KBJwt.fromKBEncode(encodedKbJwt);
     try {
       await decoded.verify(publicKey);
+    } catch (e: unknown) {
+      const error = e as SDJWTException;
+      expect(error.message).toBe('Invalid Key Binding Jwt');
+    }
+  });
+
+  test('verify with custom Verifier', async () => {
+    const { privateKey, publicKey } = Crypto.generateKeyPairSync('ed25519');
+    const testVerifier: Verifier = async (data: string, sig: string) => {
+      return Crypto.verify(
+        null,
+        Buffer.from(data),
+        publicKey,
+        Buffer.from(sig, 'base64url'),
+      );
+    };
+
+    const kbJwt = new KBJwt({
+      header: {
+        typ: KB_JWT_TYP,
+        alg: 'EdDSA',
+      },
+      payload: {
+        iat: 1,
+        aud: 'aud',
+        nonce: 'nonce',
+        _sd_hash: 'hash',
+      },
+    });
+
+    const encodedKbJwt = await kbJwt.sign(privateKey);
+    const decoded = KBJwt.fromKBEncode(encodedKbJwt);
+    const verified = await decoded.verifyWithVerifier(testVerifier);
+    expect(verified).toBe(true);
+  });
+
+  test('verify failed with custom Verifier', async () => {
+    const { privateKey, publicKey } = Crypto.generateKeyPairSync('ed25519');
+    const testVerifier: Verifier = async (data: string, sig: string) => {
+      return Crypto.verify(
+        null,
+        Buffer.from(data),
+        publicKey,
+        Buffer.from(sig, 'base64url'),
+      );
+    };
+
+    const kbJwt = new KBJwt({
+      header: {
+        typ: KB_JWT_TYP,
+        alg: 'EdDSA',
+      },
+      payload: {
+        iat: 1,
+        aud: 'aud',
+        nonce: 'nonce',
+        _sd_hash: '',
+      },
+    });
+
+    const encodedKbJwt = await kbJwt.sign(privateKey);
+    const decoded = KBJwt.fromKBEncode(encodedKbJwt);
+    try {
+      await decoded.verifyWithVerifier(testVerifier);
     } catch (e: unknown) {
       const error = e as SDJWTException;
       expect(error.message).toBe('Invalid Key Binding Jwt');
