dcb76cea354c87d1ab1211fb26b7f663f9d0242e
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-14 21:58:47 +0900

    Merging Process #1 from sd-jwt-ts (#60)
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>
    Signed-off-by: Lukas <Lukas@hopae.io>

--- src/decoy.ts
+++ src/decoy.ts
@@ -1,13 +1,12 @@
-import { Base64Url } from './base64url';
-import { generateSalt, digest , hexToB64Url} from './crypto';
-import { Hasher, SaltGenerator } from './type';
+import { HasherAndAlg, SaltGenerator } from './type';
+import { Uint8ArrayToBase64Url } from './base64url';
 
 export const createDecoy = async (
-  hasher: Hasher = digest,
-  saltGenerator: SaltGenerator = generateSalt,
+  hash: HasherAndAlg,
+  saltGenerator: SaltGenerator,
 ): Promise<string> => {
-  const salt = saltGenerator(16);
-  const decoyHexString = await hasher(salt);
-  const decoy = hexToB64Url(decoyHexString)
-  return decoy;
+  const { hasher, alg } = hash;
+  const salt = await saltGenerator(16);
+  const decoy = await hasher(salt, alg);
+  return Uint8ArrayToBase64Url(decoy);
 };
