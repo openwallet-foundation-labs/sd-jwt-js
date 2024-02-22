d7b639266b57a9bb2c10307fa3f8b93eb1c775ac
Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
2024-02-22 17:47:34 +0100

    fix: limit tests to package folder
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>

--- vitest.shared.js
+++ vitest.shared.js
@@ -2,10 +2,6 @@ import { defineProject, mergeConfig } from 'vitest/config';
 export const browserConfig = defineProject({
   test: {
     globals: true,    
-    coverage: {
-      exclude: ['examples/**'],
-      reporter: ['json'],
-    },
     environment: 'jsdom',
   },
 });
@@ -13,11 +9,6 @@ export const browserConfig = defineProject({
 export const nodeConfig = defineProject({
   test: {
     globals: true,    
-    coverage: {
-      //TODO: the exclude is not working, therefore the coverage result are not correct.
-      exclude: ['examples/**'],
-      reporter: ['json'],
-    },
     environment: 'node',
   },
 });
