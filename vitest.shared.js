853f09356506f2ee39c7e37b3a3062cef65d4959
Mirko Mollik <mirkomollik@gmail.com>
2024-02-22 15:45:41 +0100

    style: formatted code with biome (#82)
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>
    Co-authored-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>

--- vitest.shared.js
+++ vitest.shared.js
@@ -1,27 +1,27 @@
 import { defineProject, mergeConfig } from 'vitest/config';
 export const browserConfig = defineProject({
-    test: {
-      globals: true,
-      exclude: ['**/*e2e.spec.ts'],      
-      coverage: {        
-        exclude: ['examples/**'],
-        reporter: ['json'],
-      },
-      environment: 'jsdom',
+  test: {
+    globals: true,
+    exclude: ['**/*e2e.spec.ts'],
+    coverage: {
+      exclude: ['examples/**'],
+      reporter: ['json'],
     },
-  });
-  
-  export const nodeConfig = defineProject({
-    test: {
-      globals: true,
-      exclude: ['**/*e2e.spec.ts'],            
-      coverage: {
-        //TODO: the exclude is not working, therefore the coverage result are not correct.
-        exclude: ['examples/**'],
-        reporter: ['json'],
-      },
-      environment: 'node',
+    environment: 'jsdom',
+  },
+});
+
+export const nodeConfig = defineProject({
+  test: {
+    globals: true,
+    exclude: ['**/*e2e.spec.ts'],
+    coverage: {
+      //TODO: the exclude is not working, therefore the coverage result are not correct.
+      exclude: ['examples/**'],
+      reporter: ['json'],
     },
-  });
-  
-  export const allEnvs = mergeConfig(browserConfig, nodeConfig);
+    environment: 'node',
+  },
+});
+
+export const allEnvs = mergeConfig(browserConfig, nodeConfig);
