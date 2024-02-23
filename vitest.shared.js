9d374eb61fbaacfa820d912029d1b1988753a794
Mirko Mollik <mirkomollik@gmail.com>
2024-02-23 01:40:02 +0100

    include e2e tests (#86)
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
    Co-authored-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>

--- vitest.shared.js
+++ vitest.shared.js
@@ -2,11 +2,6 @@ import { defineProject, mergeConfig } from 'vitest/config';
 export const browserConfig = defineProject({
   test: {
     globals: true,
-    exclude: ['**/*e2e.spec.ts'],
-    coverage: {
-      exclude: ['examples/**'],
-      reporter: ['json'],
-    },
     environment: 'jsdom',
   },
 });
@@ -14,12 +9,6 @@ export const browserConfig = defineProject({
 export const nodeConfig = defineProject({
   test: {
     globals: true,
-    exclude: ['**/*e2e.spec.ts'],
-    coverage: {
-      //TODO: the exclude is not working, therefore the coverage result are not correct.
-      exclude: ['examples/**'],
-      reporter: ['json'],
-    },
     environment: 'node',
   },
 });
