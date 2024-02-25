a77a827c3b9baa9dd780dd06991c287e721fb997
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-25 12:26:30 +0900

    feat: add crypto-nodejs Ed25519 and ES256
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- packages/node-crypto/src/crypto.ts
+++ packages/node-crypto/src/crypto.ts
@@ -1,4 +1,12 @@
-import { createHash, randomBytes } from 'crypto';
+import {
+  createHash,
+  randomBytes,
+  sign,
+  verify,
+  createPrivateKey,
+  createPublicKey,
+  generateKeyPairSync,
+} from 'crypto';
 
 export const generateSalt = (length: number): string => {
   if (length <= 0) {
@@ -19,3 +27,60 @@ export const digest = (data: string, algorithm = 'SHA-256'): Uint8Array => {
 
 const toNodeCryptoAlg = (hashAlg: string): string =>
   hashAlg.replace('-', '').toLowerCase();
+
+const getSigner = (privateKeyPEM: string) => {
+  const privateKey = createPrivateKey(privateKeyPEM);
+  return async (data: string) => {
+    const sig = sign(null, Buffer.from(data), privateKey);
+    return Buffer.from(sig).toString('base64url');
+  };
+};
+
+const getVerifier = (publicKeyPEM: string) => {
+  const publicKey = createPublicKey(publicKeyPEM);
+  return async (data: string, sig: string) => {
+    return verify(
+      null,
+      Buffer.from(data),
+      publicKey,
+      Buffer.from(sig, 'base64url'),
+    );
+  };
+};
+
+export const Ed25519 = {
+  alg: 'EdDSA',
+  generateKeyPair: () => {
+    return generateKeyPairSync('ed25519', {
+      publicKeyEncoding: {
+        type: 'spki', // Recommended format for public key
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8', // Recommended format for private key
+        format: 'pem',
+      },
+    });
+  },
+  getSigner,
+  getVerifier,
+};
+
+export const ES256 = {
+  alg: 'ES256',
+  generateKeyPair: () => {
+    return generateKeyPairSync('ec', {
+      namedCurve: 'P-256',
+      publicKeyEncoding: {
+        type: 'spki', // Recommended format for public key
+        format: 'pem',
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8', // Recommended format for private key
+        format: 'pem',
+      },
+    });
+  },
+  getSigner,
+  getVerifier,
+};
--- packages/node-crypto/src/test/crypto.spec.ts
+++ packages/node-crypto/src/test/crypto.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { generateSalt, digest } from '../crypto';
+import { generateSalt, digest, ES256, Ed25519 } from '../crypto';
 
 describe('This file is for utility functions', () => {
   test('crypto', () => {
@@ -30,5 +30,39 @@ describe('This file is for utility functions', () => {
     const s1 = await digest(payload, 'SHA512');
     expect(s1).toBeDefined();
     expect(s1.length).toBe(64);
+  });
+
+  test('Ed25519', async () => {
+    const { privateKey, publicKey } = Ed25519.generateKeyPair();
+    expect(privateKey).toBeDefined();
+    expect(publicKey).toBeDefined();
+    expect(typeof privateKey).toBe('string');
+    expect(typeof publicKey).toBe('string');
+
+    const data =
+      'In cryptography, a salt is random data that is used as an additional input to a one-way function that hashes data, a password or passphrase.';
+    const signature = await Ed25519.getSigner(privateKey)(data);
+    expect(signature).toBeDefined();
+    expect(typeof signature).toBe('string');
+
+    const result = await Ed25519.getVerifier(publicKey)(data, signature);
+    expect(result).toBe(true);
+  });
+
+  test('ES256', async () => {
+    const { privateKey, publicKey } = ES256.generateKeyPair();
+    expect(privateKey).toBeDefined();
+    expect(publicKey).toBeDefined();
+    expect(typeof privateKey).toBe('string');
+    expect(typeof publicKey).toBe('string');
+
+    const data =
+      'In cryptography, a salt is random data that is used as an additional input to a one-way function that hashes data, a password or passphrase.';
+    const signature = await ES256.getSigner(privateKey)(data);
+    expect(signature).toBeDefined();
+    expect(typeof signature).toBe('string');
+
+    const result = await ES256.getVerifier(publicKey)(data, signature);
+    expect(result).toBe(true);
   });
 });
