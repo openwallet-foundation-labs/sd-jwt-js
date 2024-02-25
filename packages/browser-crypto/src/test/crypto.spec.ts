a566ce4ff784ce45b5e3302fe6c327f48f1c0e3b
Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
2024-02-25 21:44:35 +0100

    print if used as browser or node
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>

--- packages/browser-crypto/src/test/crypto.spec.ts
+++ packages/browser-crypto/src/test/crypto.spec.ts
@@ -2,38 +2,35 @@ import { describe, expect, test } from 'vitest';
 import { generateSalt, digest, getHasher } from '../index';
 
 describe('This file is for utility functions', () => {
-  test('crypto', () => {
-    expect('1').toStrictEqual('1');
+  test('generateSalt', async () => {
+    console.log(typeof window === 'undefined' ? 'node!' : 'browser!');
+    const salt = generateSalt(8);
+    expect(salt).toBeDefined();
+    expect(salt.length).toBe(8);
   });
 
-  // test('generateSalt', async () => {
-  //   const salt = generateSalt(8);
-  //   expect(salt).toBeDefined();
-  //   expect(salt.length).toBe(8);
-  // });
+  test('generateSalt 0 length', async () => {
+    const salt = generateSalt(0);
+    expect(salt).toBeDefined();
+    expect(salt.length).toBe(0);
+  });
 
-  // test('generateSalt 0 length', async () => {
-  //   const salt = generateSalt(0);
-  //   expect(salt).toBeDefined();
-  //   expect(salt.length).toBe(0);
-  // });
+  test('digest', async () => {
+    const payload = 'test1';
+    const s1 = await digest(payload);
+    expect(s1).toBeDefined();
+    expect(s1.length).toBe(32);
+  });
 
-  // test('digest', async () => {
-  //   const payload = 'test1';
-  //   const s1 = await digest(payload);
-  //   expect(s1).toBeDefined();
-  //   expect(s1.length).toBe(32);
-  // });
+  test('digest', async () => {
+    const payload = 'test1';
+    const s1 = await digest(payload, 'SHA-512');
+    expect(s1).toBeDefined();
+    expect(s1.length).toBe(64);
+  });
 
-  // test('digest', async () => {
-  //   const payload = 'test1';
-  //   const s1 = await digest(payload, 'SHA-512');
-  //   expect(s1).toBeDefined();
-  //   expect(s1.length).toBe(64);
-  // });
-
-  // test('get hasher', async () => {
-  //   const hash = await getHasher('SHA-512')('test1');
-  //   expect(hash).toBeDefined();
-  // });
+  test('get hasher', async () => {
+    const hash = await getHasher('SHA-512')('test1');
+    expect(hash).toBeDefined();
+  });
 });
