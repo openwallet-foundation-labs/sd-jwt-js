aa47a0fb0d47b02958f2b620785ca205af07e906
Mirko Mollik <mirkomollik@gmail.com>
2024-08-16 14:25:43 +0200

    chore: format
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- packages/types/src/type.ts
+++ packages/types/src/type.ts
@@ -147,15 +147,15 @@ type Frame<Payload> = Payload extends Array<infer U>
     ? Record<number, Frame<U>> & SD<Payload> & DECOY
     : SD<Payload> & DECOY
   : Payload extends Record<string, unknown>
-  ? NonNever<
-      {
-        [K in keyof Payload]?: Payload[K] extends object
-          ? Frame<Payload[K]>
-          : never;
-      } & SD<Payload> &
-        DECOY
-    >
-  : SD<Payload> & DECOY;
+    ? NonNever<
+        {
+          [K in keyof Payload]?: Payload[K] extends object
+            ? Frame<Payload[K]>
+            : never;
+        } & SD<Payload> &
+          DECOY
+      >
+    : SD<Payload> & DECOY;
 
 /**
  * This is a disclosureFrame type that is used to represent the structure of what is being disclosed.
